e759322c64e256b75d84da94691ea0439f6bd7de
dnaismyth <naismythdayna@gmail.com>
2016-09-11 14:15:35 -0700

    Initial testing and module setup

---
+++ flow_frontend/app.js
@@ -0,0 +1,1 @@
+var flowApp = angular.module("flowApp", []);
---
+++ flow_frontend/user_controller.js
@@ -0,0 +1,14 @@
+// Controllers for user
+
+// Load in app module
+$.getScript('app.js', function()
+{
+	
+	flowApp.controller('Hello', function($scope, $http) {
+    	$http.get('http://localhost:8080/api/signup/hello').
+        	then(function(response) {
+            	$scope.user = response.data;
+        	});
+	});
+
+});
