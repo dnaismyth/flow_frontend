a309aa01b1a65d0529c57754a99da68029ef8dd8
dnaismyth <naismythdayna@gmail.com>
2016-09-30 22:30:52 -0700

    start to add url templates for login

--- flow_frontend/flow_client/app/app.js
+++ flow_frontend/flow_client/app/app.js
@@ -1,5 +1,18 @@
 
-var flowApp = angular.module('flowApp', ['ngResource', 'ngRoute', 'ngCookies']);
+var flowApp = angular.module('flowApp', ['ngResource','ngRoute','ngCookies']);
+
+flowApp.config(function ($routeProvider){
+
+    $routeProvider
+
+    .when('/login', {
+        templateUrl: 'pages/login.html',
+        controller:  'secureCtrl'
+    }).when('/second',{
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+});
 
 flowApp.controller('Hello', function($scope, $http) {
     $http.get('http://localhost:8080/api/register/hello').
@@ -15,9 +28,9 @@ flowApp.controller('secureCtrl',
             grant_type:"password",
             username: "",
             password: "",
-            client_id: "clientIdPassword"
+            client_id: "mediacenter"
         };
-        $scope.encoded = btoa("clientIdPassword:secret");
+        $scope.encoded = btoa("mediacenter:secret");
         $scope.login = function() {
             var req = {
                 method: 'POST',
@@ -32,7 +45,15 @@ flowApp.controller('secureCtrl',
                 $http.defaults.headers.common.Authorization =
                     'Bearer ' + data.data.access_token;
                 $cookies.put("access_token", data.data.access_token);
-                window.location.href="www.google.ca";
+                window.location.href="#/second";
             });
         }
     });
+
+flowApp.controller('firstController', ['$scope', '$log', function($scope, $log){
+
+}]);
+
+flowApp.controller('secondController', ['$scope', '$log', function($scope, $log){
+
+}]);
