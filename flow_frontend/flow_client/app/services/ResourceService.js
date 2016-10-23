473e4b9cadab436a0ba865e6156087f07c1aa551
dnaismyth <naismythdayna@gmail.com>
2016-10-23 10:58:22 -0700

    Update resource service to include password reset api

--- flow_frontend/flow_client/app/services/ResourceService.js
+++ flow_frontend/flow_client/app/services/ResourceService.js
@@ -15,11 +15,23 @@
     function ResourceService($http) {
         var service = {}
         service.GetS3Token = GetS3Token;
+        service.SendPasswordResetEmail = SendPasswordResetEmail;
+        service.FinishPasswordReset = FinishPasswordReset;
         return service;
 
         // Used to retrieve a temporary token for the user to upload media during their logged in session
         function GetS3Token(){
             return $http.get('http://localhost:8080/api/resources/s3token').then(handleSuccess, handleError('Error retrieving s3 token.'));
+        }
+
+        // Send password reset e-mail to the address provided
+        function SendPasswordResetEmail(email){
+            return $http.post('http://localhost:8080/api/resources/passwordreset', email).then(handleSuccess, handleError('Error sending password reset e-mail.'));
+        }
+
+        // Allow for a user to update and finish their password reset
+        function FinishPasswordReset(password){
+            return $http.post('http://localhost:8080/api/resources/finish-reset-password', password).then(handleSuccess, handleError('error completing reset password request.'));
         }
 
         // private functions
